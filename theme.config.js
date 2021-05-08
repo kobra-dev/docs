a2100cb2494e1db3b665acbba58cb20b67f1391a
merlin04 <Merlin04@users.noreply.github.com>
2021-05-08 14:12:31 -0700

    Change Nextra to Kobra

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/kobra-dev/docs-nextra',
+  titleSuffix: ' – Kobra Docs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Kobra</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        Documentation
       </span>
     </>
   ),
@@ -15,15 +15,12 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="description" content="Kobra documentation" />
+      <meta name="og:description" content="Kobra documentation" />
+      <meta name="twitter:site:domain" content="docs.kobra.dev" />
+      <meta name="twitter:url" content="https://docs.kobra.dev" />
+      <meta name="og:title" content="Kobra Docs" />
+      <meta name="apple-mobile-web-app-title" content="Kobra Docs" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -61,5 +58,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Kobra.</>,
 }
